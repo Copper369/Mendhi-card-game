a6af6bf515263cab2aca5a33b109d4e27ec8c7a5
Ayush Karnewar <ayushkarnewar369@gmail.com>
2026-02-22 14:45:35 +0530

    Add 10 of Clubs as favicon

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <title>Mendhikot Online - Multiplayer Card Game</title>
+        <link rel="icon" href="/favicon.jpg" type="image/jpeg" />
+        <link rel="apple-touch-icon" href="/favicon.jpg" />
       </Head>
       <Component {...pageProps} />
     </>
